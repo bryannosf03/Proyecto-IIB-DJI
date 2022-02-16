c504b811222cdeb0a243f59be2ac4345a8a3f307
Bryan <bryan.flores04@epn.edu.ec>
2022-02-16 10:14:38 -0500

    Clase para Continentes

---
+++ JuegoSerioIIB/Assets/Scripts/Continentes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Continentes : MonoBehaviour
+{
+    public static string[] America = {"Argentina","Ecuador","Colombia"};
+    public static string[] Europa = {"España","Italia","Alemania"};
+    public static string[] Asia = {"China","Japón","Thailandia"};
+    public static string[] Oceania = {"Nueva Zelanda","Australia","Isla Salomón"};
+    public static string[] Africa = {"Egipto","Sudáfrica","Siria"};
+    
+}
