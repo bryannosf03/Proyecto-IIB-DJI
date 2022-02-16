972e6ada5b49551aaec70594e31aad922b579a92
Francisco García <franciscojavgm@gmail.com>
2022-02-16 17:09:07 -0500

    Revert "Clase para Continentes"
    This reverts commit c504b811222cdeb0a243f59be2ac4345a8a3f307.

--- JuegoSerioIIB/Assets/Scripts/GestionadorDeJuego.cs
+++ JuegoSerioIIB/Assets/Scripts/GestionadorDeJuego.cs
@@ -13,28 +13,11 @@ public class GestionadorDeJuego : MonoBehaviour
         string[] opciones={"Ecuador", "Guayakill","El Sur","HELL"};
         Asignador.Instancia.mostrarOpciones(opciones);
         Asignador.Instancia.mostrarBandera(imagen,bandera);
-       // mostrarBandera(imagen,bandera);
-        //mostrarOpciones(opciones);
     }
 
-    public void mostrarBandera(Image imagenUI, Sprite banderaACambiar){
-        imagenUI.sprite = banderaACambiar;
-    }
-
-    public void mostrarOpciones(string[] opciones){
-        GameObject boton1 = GameObject.Find("Opcion1");
-        GameObject boton2 = GameObject.Find("Opcion2");
-        GameObject boton3 = GameObject.Find("Opcion3");
-        GameObject boton4 = GameObject.Find("Opcion4");
-
-        boton1.GetComponentInChildren<Text>().text = opciones[0];
-        boton2.GetComponentInChildren<Text>().text = opciones[1];
-        boton3.GetComponentInChildren<Text>().text = opciones[2];
-        boton4.GetComponentInChildren<Text>().text = opciones[3];
-    }
     //método para hacer un nivel y llamará a: 
 
-    //leer sprites(banderas) de la carpeta de assets según el nivel
+    //leer sprites de la carpeta de assets según el nivel
 
     //seleccionar 4 opciones una de ellas correcta
 
