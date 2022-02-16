40796dbffe455c92cf746ed3a3f39bd180db60c6
Francisco García <franciscojavgm@gmail.com>
2022-02-16 17:07:00 -0500

    Revert "Update/Randomico"
    This reverts commit 9bde1c81adaa50b4104c54cf9ce7dec978f451be.

--- JuegoSerioIIB/Assets/Scripts/Continentes.cs
+++ JuegoSerioIIB/Assets/Scripts/Continentes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Continentes : MonoBehaviour
 {
-    public static string[] America = {"Argentina","Ecuador","Colombia","Brasil","Perú","Bolivia","Chile"};
+    public static string[] America = {"Argentina","Ecuador","Colombia"};
     public static string[] Europa = {"España","Italia","Alemania"};
     public static string[] Asia = {"China","Japón","Thailandia"};
     public static string[] Oceania = {"Nueva Zelanda","Australia","Isla Salomón"};
