9bde1c81adaa50b4104c54cf9ce7dec978f451be
Bryan <bryan.flores04@epn.edu.ec>
2022-02-16 11:30:34 -0500

    Update/Randomico
    -Metodo para mostrar banderas de acuerdo a su continente con sprite

--- JuegoSerioIIB/Assets/Scripts/GestionadorDeJuego.cs
+++ JuegoSerioIIB/Assets/Scripts/GestionadorDeJuego.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+
 public class GestionadorDeJuego : MonoBehaviour
 {
     public Sprite bandera;
     public Image imagen;
+    public SpriteRenderer spriteRenderer;
+
+    private string opcionCorrecta="";
 
     void Start()
     {
-        string[] opciones={"Ecuador", "Guayakill","El Sur","HELL"};
-        Asignador.Instancia.mostrarOpciones(opciones);
-        Asignador.Instancia.mostrarBandera(imagen,bandera);
+        //Asignador.Instancia.mostrarOpciones(opciones);
+        //Asignador.Instancia.mostrarBandera(imagen,bandera);
        // mostrarBandera(imagen,bandera);
-        //mostrarOpciones(opciones);
+       obtenerBandera();
+       string[] opciones = seleccionarOpciones();
+        mostrarOpciones(opciones);
     }
 
     public void mostrarBandera(Image imagenUI, Sprite banderaACambiar){
@@ -32,12 +37,52 @@ public class GestionadorDeJuego : MonoBehaviour
         boton3.GetComponentInChildren<Text>().text = opciones[2];
         boton4.GetComponentInChildren<Text>().text = opciones[3];
     }
-    //método para hacer un nivel y llamará a: 
 
+    
+    //método para hacer un país(y opciones) y llamará a: 
+    public void inicializarJuego(){
+        obtenerBandera();
+        
+        seleccionarOpciones();
+    }
     //leer sprites(banderas) de la carpeta de assets según el nivel
+    public void obtenerBandera(){
+            /*
+            TODO: Verificar el nivel en el que estamos(continente)
 
+            1.Tomar un país del array creado
+            2.Encontrar el sprite con ese nombre
+            3.Asignarle opcion correcta
+            4.Llamarle al mostrar bandera
+            */
+      
+        int paisAleatorio=  Random.Range(0,Continentes.America.Length);
+        string pais=Continentes.America[paisAleatorio]; //AQUÍ DEBEN IR LOS PAISES
+        spriteRenderer.sprite=Resources.Load<Sprite>(pais);
+        opcionCorrecta=pais;
+        mostrarBandera(imagen,spriteRenderer.sprite);
+
+    }
     //seleccionar 4 opciones una de ellas correcta
+    public string[] seleccionarOpciones(){
+        string[] opciones = new string[4];
 
+        int paisAleatorio =  Random.Range(0,Continentes.America.Length);
+
+        opciones[0]=Continentes.America[paisAleatorio]; 
+
+        opciones[1]=Continentes.America[paisAleatorio];
+
+        opciones[2]=Continentes.America[paisAleatorio];
+
+        opciones[3]=Continentes.America[paisAleatorio];
+
+        return opciones;
+
+    }
     //cada que selecciona verificar si es correcta o incorrecta
+    public void verificarRespuesta(string respuesta){
+
+    }
 
 }
